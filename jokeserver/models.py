6608d3b298dc8218ee2bfc65271f25f3ceca38bf
LavanyaJay <lavanyajay75@gmail.com>
2020-01-05 02:54:21 +0100

    Adding joke model

---
+++ jokeserver/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Joke(models.Model):
+    joke = models.TextField()
+    punchLine = models.TextField()
+    image = models.URLField()
